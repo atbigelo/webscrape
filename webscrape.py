c4826adf95e880d95f78c0cd7c94b4786f6defed
atbigelo <78448702+atbigelo@users.noreply.github.com>
2023-11-28 20:34:20 -0600

    Add files via upload
    V1.0

--- webscrape.py
+++ webscrape.py
@@ -1,8 +1,10 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 url = "https://www.cars.com/shopping/results/?dealer_id=&keyword=&list_price_max=&list_price_min=&makes[]=nissan&maximum_distance=all&mileage_max=&models[]=nissan-gt_r&monthly_payment=&page_size=20&sort=best_match_desc&stock_type=used&year_max=&year_min=&zip=60606"
 html = requests.get(url)
 soup = BeautifulSoup(html.content, 'html.parser')
+
 totalqty = soup.find("span", attrs={"class":"total-filter-count"})
 blackqty = soup.find("label", attrs={"for":"exterior_colors_black"})
 blueqty = soup.find("label", attrs={"for":"exterior_colors_blue"})
@@ -12,13 +14,9 @@ orangeqty = soup.find("label", attrs={"for":"exterior_colors_orange"})
 redqty = soup.find("label", attrs={"for":"exterior_colors_red"})
 silverqty = soup.find("label", attrs={"for":"exterior_colors_silver"})
 whiteqty = soup.find("label", attrs={"for":"exterior_colors_white"})
-print(totalqty)
-print(blackqty)
-print(blueqty)
-print(grayqty)
-print(greenqty)
-print(orangeqty)
-print(redqty)
-print(silverqty)
-print(whiteqty)
 
+colorqty = []
+colorqty.extend((blackqty, blueqty, grayqty, greenqty, orangeqty, redqty, silverqty, whiteqty))   
+print("The total number of GT-Rs listed for sale in the US: " + re.sub(r'<.+?>', '', str(totalqty)))
+for color in colorqty:
+    print(re.sub(r'<.+?>', '', str(color)))
